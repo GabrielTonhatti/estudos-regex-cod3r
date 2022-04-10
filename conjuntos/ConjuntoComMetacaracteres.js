0b97cd6ba72f59d8d58014cd2f4ca547aaec0b69
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-04-10 13:27:11 -0300

    Conjunto com MetaCaracteres

--- conjuntos/ConjuntoComMetacaracteres.js
+++ conjuntos/ConjuntoComMetacaracteres.js
@@ -1,3 +1,9 @@
 const texto = '.$+*?-';
 console.log(texto.match(/[+.?*$]/g)); // não precisa de escapte(\) dentro do conjunto
 console.log(texto.match(/[$-?]/g)); // isso é um intervalo (range)
+
+// Não é um intervalo (range)
+console.log(texto.match(/[$\-?]/g));
+console.log(texto.match(/[-?]/g));
+
+// pode precisar de espace (\) dentro do conjunto: - [ ] ^
